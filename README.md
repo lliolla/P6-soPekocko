1c9e0db1540566c14a6a3f76c9cc3dd6c8e2c5b6
lliolla <lliolla@hotmail.com>
2021-06-21 16:49:22 +0200

    serveur et base de données connecté - frontend connecté

---
+++ README.md
@@ -0,0 +1,9 @@
+git push -u origin main
+
+rojet n°6: Construire une API sécurisée pour une application d'avis gastronomiques
+
+Frontend Le frontend était fourni dans le cadre de ce projet Le projet a été généré avec Angular CLI version 7.0.2. Pour faire fonctionner le projet, vous devez installer node-sass à part. Démarrer ng serve pour avoir accès au serveur de développement. Rendez-vous sur http://localhost:4200/. L'application va se recharger automatiquement si vous modifiez un fichier source.
+
+Backend Les technologies utilisées pour le back: un server NodeJS, une base de données MongoDB, le framework Express et le pack Mongoose. Implémenter le fichier .env (fourni dans le dossier mongoDBConnect) à la racine du dossier backend pour la connexion à la base de données MongoDB. Démarrer avec node server ou nodemon server.
+
+Connexion L'utilisateur pour s'inscrire sur l'application doit fournir un email ainsi qu'un mot de passe contenant min 6 caractères (min 1 majuscule, 1 minuscule, 1 chiffre, pas de symboles, pas d'espaces).
---
+++ backend/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+//const saucesRoutes = require('./routes/sauce');
+//const userRoutes = require('./routes/user')
+
+const app = express();
+
+// Connexion à la base de données MongoDB
+mongoose.connect(' mongodb+srv://fanny:Kiwi260738@cluster0.rx01e.mongodb.net/cluster0?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+// Eviter les erros CORS afin que tout le monde puisse faire des requêtes
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); //accéder à l'API depuis n'importe quelle origine ( '*' )
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');// on donne l'autorisation de utiliser certaines entete
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');// et aussi sur certaines methodes
+    next();// j'appelle next() pour passer au middleware d'apres
+});
+// remplace l'appel a bodyParser qui est déprecié
+app.use(express.json());
+
+
+//app.use(mongoSanitize()); // mongo-sanitize pour prévenir les risques d'injections
+//app.use(helmet()); // helmet configure de manière appropriée des en-têtes HTTP, contient 9 fonctions middlewares
+
+
+//app.use('/images', express.static(path.join(__dirname, 'images'))); // pourque app.js serve le dossier /images 
+//app.use('/api/sauces', saucesRoutes);// pour le CRUD des sauces - se refer à ./routes/sauces.js
+//app.use('/api/auth', userRoutes);// pour l'authentification de l'utilisateur - se refer à ./routes/user.js
+
+  
+module.exports = app;// Export de l'application
